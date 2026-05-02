65d091be9dc5bd60a68982df0cc3c05a91eca469
nitakedar12-del <nitakedar12@gmail.com>
2026-05-03 00:11:24 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next'
 import { Geist, Geist_Mono, Playfair_Display } from 'next/font/google'
-// import { Analytics } from '@vercel/analytics/next'
+
 import './globals.css'
 
 const _geist = Geist({ subsets: ["latin"] });
